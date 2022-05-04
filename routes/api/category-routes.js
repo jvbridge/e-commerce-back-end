3472a581b637c3c4d2b86a34b8e5d4c319d245ed
Jonathan Bridge <jvbridge@gmail.com>
2022-05-03 17:02:27 -0700

    update category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,8 +48,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      { category_name: req.body.category_name },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    if (!categoryData) {
+      res.status(404).json({ message: "no category with that ID found!" });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
