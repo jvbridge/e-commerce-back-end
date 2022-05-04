a4b359c103db67f45f337e43f247a1bc1b4b2647
Jonathan Bridge <jvbridge@gmail.com>
2022-05-04 15:14:10 -0700

    product route finished

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,13 +23,10 @@ router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    console.log("ID: ", req.params.id);
     const productData = await Product.findOne({
       where: { id: req.params.id },
       include: [{ model: Tag }, { model: Category }],
     });
-
-    console.log("Product data: ", productData);
 
     if (!productData) {
       res.status(404).json({ message: "No product with that ID found!" });
@@ -115,8 +112,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id },
+      include: [{ model: Tag }, { model: Category }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product with that ID found!" });
+      return;
+    }
+
+    res.status(200).json({ message: "Product has been dealt with" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
