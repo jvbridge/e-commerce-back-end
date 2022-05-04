01f07e79353d464f4dcdb27c52439dec000e358e
Jonathan Bridge <jvbridge@gmail.com>
2022-05-04 14:36:16 -0700

    update the schema to support many-to-many

--- models/index.js
+++ models/index.js
@@ -13,10 +13,10 @@ Product.belongsTo(Category, {
 Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: { model: ProductTag, unique: false } });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: { model: ProductTag, unique: false } });
 
 module.exports = {
   Product,
