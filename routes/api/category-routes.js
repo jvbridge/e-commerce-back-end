2ba8d377d61bd61fc715fac3f6f90a9ea00b2dd6
Jonathan Bridge <jvbridge@gmail.com>
2022-05-03 17:06:59 -0700

    delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -66,8 +66,20 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that ID!" });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
