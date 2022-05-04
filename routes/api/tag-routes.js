75c939222705f3b216da60b7ecc67072302d5b87
Jonathan Bridge <jvbridge@gmail.com>
2022-05-03 17:17:45 -0700

    create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,8 +35,16 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
