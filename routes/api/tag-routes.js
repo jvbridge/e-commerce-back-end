b6679c6b216baf8243c460add8699e4d3106ed05
Jonathan Bridge <jvbridge@gmail.com>
2022-05-03 17:23:03 -0700

    update a tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,8 +47,22 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    if (tagData[0] === 0) {
+      res.status(404).json({ message: "no tag with that ID found!" });
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
